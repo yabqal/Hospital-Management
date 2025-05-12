7617cff559d019ce82a7b120873e73ee6b52540b
HyoketsuSenpai <Youseftela@gmail.com>
2025-05-12 07:56:07 +0300

    Give project mvc structure. Add the .htaccess file. Add Models.

---
+++ App/Model/Appointment.php
@@ -0,0 +1,20 @@
+<?php
+
+class Appointment extends Model{
+
+    function __construct()
+    {
+        parent::__construct("Appointmments");
+    }
+
+    function getAll(){
+        //fix the query based on the actual cols the table has
+        return mysqli_fetch_all(mysqli_query(Model::$db,
+               "SELECT Appointments.id, Doctors.name AS doctorName, Patients.name AS patientName, date, reason FROM Appointments
+                JOIN Doctors ON Doctors.id = Appointments.did
+                JOIN Patients ON Patients.id = Appointments.pid"
+               ),
+               MYSQLI_ASSOC);
+    }
+
+}
---
+++ App/Model/Doctor.php
@@ -0,0 +1,14 @@
+<?php
+
+class Doctor extends Model{
+
+    function __construct()
+    {
+        parent::__construct("Doctors");
+    }
+
+    function getAll(){
+        return mysqli_fetch_all(mysqli_query(Model::$db, "SELECT * FROM Doctors"), MYSQLI_ASSOC);
+    }
+    
+}
